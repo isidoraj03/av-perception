25fcc1f7ed3f2935f691a6f0fad604d017b4f776
isidoraj03 <isidora.jakovlevska@student.um.si>
2025-06-06 18:31:15 +0200

    test from another laptop

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your scripttt.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctdrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter toDDSA run the script.
